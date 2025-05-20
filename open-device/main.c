08fb6e6414d23f7131bfce580c483a7d2a328b4a
Arthur63 <arthur63@163.com>
2025-05-20 22:25:35 +0800

    update record-audio

--- open-device/main.c
+++ open-device/main.c
@@ -12,18 +12,21 @@ main (int argc, char **argv)
   AVFormatContext *fmt_ctx = NULL;
   const AVInputFormat *in_fmt = NULL;
 
+  /* 注册设备 */
   avdevice_register_all ();
 
+  /* 确定输入格式 */
   in_fmt = av_find_input_format (INPUT_FORMAT);
-  if (!in_fmt)
+  if (in_fmt == NULL)
     {
       fprintf (stderr, "Could not find input format '%s'.\n", INPUT_FORMAT);
       return 1;
     }
   printf ("Found input format '%s'.\n", in_fmt->long_name);
 
+  /* 打开设备 */
   ret = avformat_open_input (&fmt_ctx, DEVICE_NAME, in_fmt, NULL);
-  if (ret < 0)
+  if (ret != 0)
     {
       av_strerror (ret, errbuf, sizeof (errbuf));
       fprintf (stderr, "Could not open audio device: %s.\n", errbuf);
@@ -33,6 +36,8 @@ main (int argc, char **argv)
 
   int audio_stream_idx = -1;
   AVCodecParameters *codec_params = NULL;
+
+  /* 补充流信息 */
   ret = avformat_find_stream_info (fmt_ctx, NULL);
   if (ret < 0)
     {
@@ -40,6 +45,7 @@ main (int argc, char **argv)
       fprintf (stderr, "Failed to find stream information: %s.\n", errbuf);
     }
 
+  /* 找到第一个音频流及其编码参数 */
   for (unsigned i = 0; i < fmt_ctx->nb_streams; i++)
     {
       AVCodecParameters *params = fmt_ctx->streams[i]->codecpar;
@@ -50,7 +56,6 @@ main (int argc, char **argv)
 	  break;
 	}
     }
-
   if (audio_stream_idx == -1 || codec_params == NULL)
     {
       fprintf (stderr, "No audio stream detected.\n");
@@ -58,6 +63,7 @@ main (int argc, char **argv)
     }
   printf ("Audio Stream Found (Index: %d):\n", audio_stream_idx);
 
+  /* 通道数 */
   if (av_channel_layout_check (&codec_params->ch_layout))
     {
       int chans = codec_params->ch_layout.nb_channels;
@@ -67,12 +73,14 @@ main (int argc, char **argv)
 	printf ("  Channels: %s\n", chans == 1 ? "mono" : "stereo");
     }
 
+  /* 采样格式 */
   const char *sample_fmt_name = av_get_sample_fmt_name (codec_params->format);
   if (sample_fmt_name)
     printf ("  Sample Format: %s\n", sample_fmt_name);
   else
     printf ("  Sample Format: Unknown (%d)\n", codec_params->format);
 
+  /* 采样率 */
   printf ("  Sample Rate: %dHz\n", codec_params->sample_rate);
 
   avformat_close_input (&fmt_ctx);
--- record-audio/main.c
+++ record-audio/main.c
@@ -12,8 +12,10 @@ main (int argc, char **argv)
   AVFormatContext *fmt_ctx = NULL;
   const AVInputFormat *in_fmt = NULL;
 
+  /* 注册设备 */
   avdevice_register_all ();
 
+  /* 确定输入格式 */
   in_fmt = av_find_input_format (INPUT_FORMAT);
   if (in_fmt == NULL)
     {
@@ -22,6 +24,7 @@ main (int argc, char **argv)
     }
   printf ("Found input format '%s'.\n", in_fmt->long_name);
 
+  /* 打开设备 */
   ret = avformat_open_input (&fmt_ctx, DEVICE_NAME, in_fmt, NULL);
   if (ret != 0)
     {
@@ -31,7 +34,40 @@ main (int argc, char **argv)
     }
   printf ("Successfully opened audio device.\n");
 
-  AVPacket *pkt = av_packet_alloc ();
+  double time_base = 0;
+  int audio_stream_idx = -1;
+  AVStream *audio_stream = NULL;
+  AVCodecParameters *codec_params = NULL;
+
+  /* 补充流信息 */
+  ret = avformat_find_stream_info (fmt_ctx, NULL);
+  if (ret < 0)
+    {
+      av_strerror (ret, errbuf, sizeof (errbuf));
+      fprintf (stderr, "Failed to find stream information: %s.\n", errbuf);
+    }
+
+  /* 找到第一个音频流及其编码参数 */
+  for (unsigned i = 0; i < fmt_ctx->nb_streams; i++)
+    {
+      AVStream *stream = fmt_ctx->streams[i];
+      AVCodecParameters *params = stream->codecpar;
+      if (params && params->codec_type == AVMEDIA_TYPE_AUDIO)
+	{
+	  time_base = av_q2d (stream->time_base);
+	  codec_params = params;
+	  audio_stream = stream;
+	  audio_stream_idx = i;
+	  break;
+	}
+    }
+  if (audio_stream_idx == -1 || audio_stream == NULL || codec_params == NULL)
+    {
+      fprintf (stderr, "No audio stream detected.\n");
+      return 1;
+    }
+
+  /* 打开输出文件 */
   FILE *out = fopen ("out.pcm", "wb");
   if (out == NULL)
     {
@@ -39,15 +75,33 @@ main (int argc, char **argv)
       avformat_close_input (&fmt_ctx);
       return 1;
     }
-  for (int i = 1; (ret = av_read_frame (fmt_ctx, pkt)) == 0 && i <= 250; i++)
+
+  /* 读取音频包 */
+  double start_time = AV_NOPTS_VALUE;
+  AVPacket *pkt = av_packet_alloc ();
+  for (int i = 0; (ret = av_read_frame (fmt_ctx, pkt)) == 0; i++)
     {
+      /* 过滤音频包 */
+      if (pkt->stream_index != audio_stream_idx)
+	continue;
+
+      /* 初始化 start_time */
+      if (start_time == AV_NOPTS_VALUE)
+	start_time = pkt->pts;
+
+      /* 计算录制时间 */
+      double time = (pkt->pts - start_time) * time_base;
       fwrite (pkt->data, 1, pkt->size, out);
-      fflush (out);
-      printf ("No.%d packet with %d bytes.\n", i, pkt->size);
+      /* fflush (out); */
+
+      printf ("[%lfs] No.%d packet with %d bytes.\n", time, i, pkt->size);
       av_packet_unref (pkt);
+
+      if (time >= 3)
+	break;
     }
+
   fclose (out);
   av_packet_free (&pkt);
-
   avformat_close_input (&fmt_ctx);
 }
